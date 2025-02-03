47b74f187e2be6606b3a84878db13532e7c64ace
viderq <102806262+viderq@users.noreply.github.com>
2025-02-03 16:29:01 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'news-pwa-v2';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
